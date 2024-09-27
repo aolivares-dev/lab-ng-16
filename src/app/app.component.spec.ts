97ecef6de8d01e08085f86d443ce3acbfc91aa0d
Alessandro Olivares <93617639+aolivares-dev@users.noreply.github.com>
2024-09-27 00:44:02 +0000

    feat: adding test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,4 +26,32 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
   });
+
+  it('fibonacci para los primeros elementos', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.componentInstance;
+    expect(compiled.getFibonacciSequence(0)).toEqual(0);
+  });
+
+  it('fibonacci para lel elemento 2', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.componentInstance;
+    expect(compiled.getFibonacciSequence(2)).toEqual(1);
+  });
+
+  it('fibonacci para lel elemento 5', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.componentInstance;
+    expect(compiled.getFibonacciSequence(5)).toEqual(5);
+  });
+
+  it('fibonacci para lel elemento 20', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.componentInstance;
+    expect(compiled.getFibonacciSequence(20)).toEqual(6765);
+  });
 });
